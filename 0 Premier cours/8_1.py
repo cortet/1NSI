53f4d4f1eead0e9cc01d69dbbaa1d1d28f20b712
cortet <62605611+cortet@users.noreply.github.com>
2020-09-10 09:40:06 +0200

    RAZ première

--- 0 Premier cours/8_1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  2 12:15:37 2019
-
-@author: Sophie
-"""
-
-def exercice1(a):
-    return a**2-a+1
--- 0 Premier cours/8_2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  2 12:17:25 2019
-
-@author: Sophie
-"""
-
-def vitesse(distance,temps):
-    return distance/temps
